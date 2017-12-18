98869be71d03e9f2e5e7377a96aa9878b18aa265
Pedro Duarte <pedro.duarte@optimuminterfaces.com.br>
2017-12-18 17:10:00 -0200

    Implementação Seção 7 Completa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,6 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { AboutComponent } from './about/about.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 
 export const ROUTES: Routes = [
@@ -17,7 +15,7 @@ export const ROUTES: Routes = [
       {path: 'menu', component: MenuComponent},
       {path: 'reviews', component: ReviewsComponent}
     ]},
-  {path: 'order', component: OrderComponent},
+  {path: 'order', loadChildren: './order/order.module#OrderModule'},
   {path: 'order-sumary', component: OrderSumaryComponent},
-  {path: 'about', component: AboutComponent},
+  {path: 'about', loadChildren: './about/about.module#AboutModule'},
 ];
---
+++ src/app/order/order.module.ts
@@ -0,0 +1,19 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
+
+import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
+import { OrderItemsComponent } from './order-items/order-items.component';
+import { OrderComponent } from './order.component';
+
+const ROUTES: Routes = [
+    {path: '', component: OrderComponent}
+];
+
+@NgModule({
+    declarations: [OrderComponent, OrderItemsComponent, DeliveryCostsComponent],
+    imports: [ SharedModule, RouterModule.forChild(ROUTES)],
+    exports: [],
+    providers: [],
+})
+export class OrderModule {}
